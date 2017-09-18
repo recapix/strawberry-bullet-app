8d30dba7a7ffcdd036d8ec3a45c5f6b10cbe7111
Renan Marcel Paulino Leite <renan.leite@globo.com>
2017-09-18 15:35:13 -0300

    Configuração ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// Import HttpClientModule from @angular/common/http
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+//Application
 import { MyApp } from './app.component';
-import { HomePage , WalkthroughPage } from '../pages';
 
+// Pages
+import { HomePage, WalkthroughPage } from '../pages';
+
+// Components
 import { PreloadImage, BackgroundImage, NewItemComponent, ShowHideContainer, ShowHideInput, ColorRadio, CounterInput, Rating } from "../components";
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -28,7 +41,15 @@ import { PreloadImage, BackgroundImage, NewItemComponent, ShowHideContainer, Sho
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
